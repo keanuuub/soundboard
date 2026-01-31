7d0355a9189a6ce6d582714a9c8d776af7d4d578
Keanu Broughton <keanubroughton@MacBookPro.lan>
2026-01-30 19:07:10 -0500

    add finalized commands

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['boo', 'horn', 'rings', 'sus', 'wrong']
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById('buttons').appendChild(btn);
+
+    function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
+});
